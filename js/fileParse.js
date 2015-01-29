756027ed6ac738e05a3a984f3329b882a0bd3f52
jonnykry <jonnykry93@gmail.com>
2015-01-29 17:06:45 -0600

    Visual upgrades, slightly mobile supported now, added a choice for multiple paragraphs.

--- js/fileParse.js
+++ js/fileParse.js
@@ -1,31 +1,46 @@
+var getUrl = ('http://jonnykrysh.me/kanye.txt');
+
+var paragraphCount = 1;
+
 $(document).ready(function() {
 
-    //var getUrl = ('http://localhost:63342/webdev/kanyeipsum/js/kanye.txt');
-    var getUrl = ('https://github.com/jonnykry/MBDTLIG/blob/master/js/kanye.txt');
-
     $('.generate').click(function() {
-        $.get(getUrl, function(data) {
-            var input = data;
-            // console.log(data);
-            var lineData = input.split('\n');
-
-            var loremIpsum = '\t';
-
-            while(loremIpsum.length < 1500) {
-                rand = Math.floor( (Math.random() * lineData.length) + 1 );
-                randomLine = lineData[rand].trim();
-                while(randomLine === '') {
-                    rand = Math.floor( (Math.random() * lineData.length) + 1 );
-                    randomLine = lineData[rand]
-                }
-                loremIpsum += (randomLine + '. ');
-            }
-
-            console.log(loremIpsum);
-            var html = '<p>' + loremIpsum + '</p';
-            var ele = document.getElementById('ipsum');
-            ele.innerHTML = html;
-        });
+        generate(paragraphCount);
     });
 
 });
+
+function changeParagraphs() {
+    val = document.getElementById("selected").value;
+    paragraphCount = val;
+}
+
+function generate(paragraphs) {
+
+    $.get(getUrl, function(data) {
+        var input = data;
+        var lineData = input.split('\n');
+
+        var tempIpsum = '\t';
+        var finalIpsum = '\t';
+
+        for(var i = 0; i < paragraphs; i++) {
+            while (tempIpsum.length < 1500) {
+                rand = Math.floor((Math.random() * lineData.length) + 1);
+                randomLine = lineData[rand].trim();
+                while (randomLine === '') {
+                    rand = Math.floor((Math.random() * lineData.length) + 1);
+                    randomLine = lineData[rand]
+                }
+                tempIpsum += (randomLine + '. ');
+            }
+            finalIpsum += (tempIpsum + ('\n\n<br><br>'));
+            tempIpsum = '';
+        }
+
+        var html = '<p>' + finalIpsum + '</p';
+        var ele = document.getElementById('ipsum');
+        ele.innerHTML = html;
+    });
+
+}
