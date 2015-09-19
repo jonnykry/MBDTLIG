66064a5186bbc424631dd0ce1ab1f434bb56e580
jonnykry <jonnykry93@gmail.com>
2015-09-19 18:19:03 -0500

    bug fix and code clean-up

--- js/fileParse.js
+++ js/fileParse.js
@@ -1,5 +1,3 @@
-var getUrl = ('http://jonnykrysh.me/kanye.txt');
-
 var paragraphCount = 1;
 
 $(document).ready(function() {
@@ -17,22 +15,26 @@ function changeParagraphs() {
 
 function generate(paragraphs) {
 
+    var paragraphLength = 1500;
+    var getUrl = '../../kanye.txt';
+
     $.get(getUrl, function(data) {
-        var input = data;
-        var lineData = input.split('\n');
+        var lineData = data.split('\n');
 
         var tempIpsum = '\t';
         var finalIpsum = '\t';
 
         for(var i = 0; i < paragraphs; i++) {
-            while (tempIpsum.length < 1500) {
-                rand = Math.floor((Math.random() * lineData.length) + 1);
-                randomLine = lineData[rand].trim();
-                while (randomLine === '') {
-                    rand = Math.floor((Math.random() * lineData.length) + 1);
-                    randomLine = lineData[rand]
+            var randomLine = '';
+
+            // create a paragraph and avoid empty lines in the text
+            while (tempIpsum.length < paragraphLength) {
+                // select a random Yeezy bar *flame emoji*
+                var rand = Math.floor((Math.random() * lineData.length) + 1);
+                randomLine = typeof lineData[rand] !== 'undefined' ? lineData[rand].trim() : '';
+                if (randomLine !== '') {
+                    tempIpsum += (randomLine + '. ');
                 }
-                tempIpsum += (randomLine + '. ');
             }
             finalIpsum += (tempIpsum + ('\n\n<br><br>'));
             tempIpsum = '';
